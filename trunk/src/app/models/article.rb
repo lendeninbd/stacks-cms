959ff7e0280541a5cdeb32ec346a72bcf150417b
stack <stack@836e1a02-0329-0410-8fd7-bd3773f666a6>
2007-02-20 05:07:14 +0000

    * Fixed the feed for validation * Created headers for the different sections

--- trunk/src/app/models/article.rb
+++ trunk/src/app/models/article.rb
@@ -2,11 +2,6 @@ class Article < Document
   
   validates_uniqueness_of :title
   
-  def before_destory
-    # Never kill the Main Page
-    raise "You can't destroy the Main Page" if self.title == 'Main Page'
-  end
-  
   def is_on_index?
     false
   end
